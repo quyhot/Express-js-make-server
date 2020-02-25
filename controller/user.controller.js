3328326324af08caf2bbc23aee92d5381f9aebed
quyhot <quyhot1@gmail.com>
2020-02-25 15:19:41 +0700

    lesson 13 on codersX

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -27,20 +27,6 @@ module.exports.usersView = function(req, res){
 
 module.exports.createUser = function(req, res){
 	req.body.id = shortid.generate();
-	var errors = [];
-	if(!req.body.name){
-		errors.push('Name is required');
-	}
-	if(!req.body.phone){
-		errors.push('Phone is required');
-	}
-	if(errors.length){
-		res.render('users/create', {
-			errors: errors,
-			values: req.body
-		})
-		return;
-	}
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 };
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var validate = require('../validate/user.validate');
 var controller = require('../controller/user.controller.js');
 
 router.get('/', controller.getUsers);
 
 router.get('/search', controller.search);
 
-router.get('/create', controller.linkToCreate)
+router.get('/create', controller.linkToCreate);
 
-router.get('/:id', controller.usersView)
+router.get('/:id', controller.usersView);
 
-router.post('/create', controller.createUser)
+router.post('/create', validate.createUser, controller.createUser);
 
 module.exports = router;
